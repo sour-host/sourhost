4d5eb211222b990f021406a1b2ee0b85f12ca748
Robyn Harrel <robynharrel851@gmail.com>
2025-10-23 10:48:07 -0500

    Update Plans.tsx

--- components/Plans.tsx
+++ components/Plans.tsx
@@ -446,9 +446,9 @@ export default function Hero() {
         <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-6 px-4 sm:px-6 lg:px-8 place-items-start">
           {plans.map((plan) => (
             <motion.div key={plan.id} className={`bg-gray-800 backdrop-blur-xl rounded-xl border border-white/5 p-6 hover:border-[#00b72f] hover:-translate-y-2 transform transition-all duration-300 group
-             ${isPopular ? 'ring-2 ring-offset-2 ring-[#00c224]' : 'ring-gray-800'} border ${isPopular ? 'border-[#00c224]' : 'border-gray-800'}`}
+             ${plan.isPopular ? 'ring-2 ring-offset-2 ring-[#00c224]' : 'ring-gray-800'} border ${plan.isPopular ? 'border-[#00c224]' : 'border-gray-800'}`}
         >
-            {isPopular && (
+            {plan.isPopular && (
               <div className="absolute -top-3 left-1/2 -translate-x-1/2">
                   <div className="inline-flex items-center gap-2 bg-[#00c224] text-white px-3 py-1 rounded-full text-sm font-semibold shadow-md">
                       Most Popular
