181fc3014bf9fa3c12dd67efd84eacd264d257a0
Robyn Harrel <robynharrel851@gmail.com>
2025-10-29 08:57:38 -0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -54,7 +54,7 @@ const Footer = () => {
   ];
 
   return (
-    <footer className="bg-gray-800 text-white">
+    <footer className="bg-white/10 text-white">
 
       {/* Main Footer Content */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
