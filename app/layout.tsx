e5ef734da4e8ca7d8aa468226d1269ee8bc771d1
allexanderxha <allexanderxha@icloud.com>
2025-11-03 09:21:37 +0100

    Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Oxanium } from 'next/font/google';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import './globals.css';
+import { Analytics } from "@vercel/analytics/next"
 
 const oxanium = Oxanium({ subsets: ['latin'] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Header />
         <main className="max-w-7xl mx-auto min-h-[calc(100vh-140px)]">
           {children}
+          <Analytics />
         </main>
         <Footer />
       </body>
