9a25af9da9d646ba97db93c3faa192a55f2b6c6d
allexanderxha <allexanderxha@icloud.com>
2025-11-07 18:41:14 +0100

    Dashboard API Route

--- app/api/dashboard/route.ts
+++ app/api/dashboard/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 // Using a GET request.
 export async function GET() {
   return NextResponse.json({
-    message: 'Hello from Sour.Host!',
+    message: 'Hello from AEPLO.COM!',
     success: true,
     data: {
       stats: {
